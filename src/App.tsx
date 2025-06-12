56bdb8abea2fba68e5171a8d5d047a344909ab9f
Ryan Godson Mathuram <168239412+ryanMathram@users.noreply.github.com>
2025-06-12 00:47:17 -0500

    Add UserProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { UserProvider } from './contexts/UserContext'
 import { Layout } from './components/layout/Layout'
 import { Home } from './pages/Home'
 import { PostDetail } from './pages/PostDetail'
@@ -10,20 +11,22 @@ import { BrainMate } from './pages/BrainMate'
 function App() {
   return (
     <ThemeProvider>
-      <Router>
-        <div className="min-h-screen bg-neutral-50 dark:bg-neutral-900">
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="post/:postId" element={<PostDetail />} />
-              <Route path="leaderboard" element={<Leaderboard />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="brainmate" element={<BrainMate />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </Router>
+      <UserProvider>
+        <Router>
+          <div className="min-h-screen bg-neutral-50 dark:bg-neutral-900">
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="post/:postId" element={<PostDetail />} />
+                <Route path="leaderboard" element={<Leaderboard />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="brainmate" element={<BrainMate />} />
+              </Route>
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+        </Router>
+      </UserProvider>
     </ThemeProvider>
   )
 }
